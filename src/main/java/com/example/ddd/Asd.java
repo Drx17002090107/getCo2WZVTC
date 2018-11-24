9568b15d04777d99c000c824ad65a2a4e6e4dcac
ZN_nick <znnznick@gmail.com>
2018-11-24 20:29:57 +0800

    111

---
+++ src/main/java/com/example/ddd/Asd.java
@@ -0,0 +1,14 @@
+package com.example.ddd;
+
+
+public class Asd {
+    String aa;
+
+    public String getAa() {
+        return aa;
+    }
+
+    public void setAa(String aa) {
+        this.aa = aa;
+    }
+}
--- src/main/java/com/example/ddd/BaseController.java
+++ src/main/java/com/example/ddd/BaseController.java
@@ -1,5 +1,6 @@
 package com.example.ddd;
 
+import com.alibaba.fastjson.JSON;
 import com.fasterxml.jackson.databind.util.JSONPObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -10,10 +11,13 @@ import java.util.Map;
 @RestController
 public class BaseController {
     @PostMapping("/ss")
-    public @ResponseBody Map<String,String> aaa(@RequestBody String aa){
+    public  Map<String,String> aaa(@RequestBody String aa){
         Map<String,String> map = new HashMap<>();
 
-        if ("co2".equals(aa)){
+
+            Asd bb = JSON.parseObject(aa,Asd.class);
+
+        if ("co2".equals(bb.getAa())){
             double min = 0.01;//最小值
             double max = 10000;//总和
             int scl =  2;//小数最大位数
@@ -21,7 +25,9 @@ public class BaseController {
             Double one = Math.floor((Math.random() * (max - min) + min) * pow) / pow;
             map.put("RESULT","S");
             map.put("SS",one.toString());
+            return map;
         }
+        map.put("RESULT","F");
         return map;
     }
 }
